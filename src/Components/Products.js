e022921f7eda67e6f78217f9d39595f8dcce09bc
amitkr2002 <kmr.amit92089@gmail.com>
2023-04-22 23:30:51 +0530

    UseState

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,10 +1,18 @@
+import { useState } from "react";
 
 function Products(props){
     // const itemsName="JBH";
+    const [title1 ,setName] = useState(props.name);
+    function clickHandler(){
+        setName("Hello Amit Kumar Welcome");
+        console.log("click hare");
+      }
+
     return(
         <div>
-            {props.name}
+            {title1}
             {props.children}
+            <button onClick={clickHandler}>Click Here</button>
         </div>
     );
 }
