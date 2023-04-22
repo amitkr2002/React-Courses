d6576658418e3ccf9a79e8a06e66480b84218477
amitkr2002 <kmr.amit92089@gmail.com>
2023-04-22 19:43:56 +0530

    we are add the one main component file card and all file under the components file

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import './App.css';
-import Products from './Components/Products';
-import ItemDate from './Components/ItemDate';
+import "./App.css";
+import Products from "./Components/Products";
+import ItemDate from "./Components/ItemDate";
 
 function App() {
-    const response=[
-      {
-        ItemName :"Excel",
-        ItemDay:"20",
-        ItemYear:"2002",
-      },
-      {
-        ItemName :"Excel2",
-        ItemDay:"203",
-        ItemYear:"20022",
-      },
-    ]
+  const response = [
+    {
+      ItemName: "Excel",
+      ItemDay: "20",
+      ItemYear: "2002",
+    },
+    {
+      ItemName: "Excel2",
+      ItemDay: "203",
+      ItemYear: "20022",
+    },
+  ];
   return (
     <div className="App">
-    <Products name={response[0].ItemName} >
-      How to pass the children value
-    </Products>
-    <ItemDate day  ={response[0].ItemDay} year={response[1].ItemYear}></ItemDate>
-    <Products name={response[1].ItemName} ></Products>
-    <ItemDate day  ={response[1].ItemDay} year={response[1].ItemYear}></ItemDate>
+      <card>
+        <Products name={response[0].ItemName}>
+          How to pass the children value
+        </Products>
+        <ItemDate
+          day={response[0].ItemDay}
+          year={response[1].ItemYear}
+        ></ItemDate>
+        <Products name={response[1].ItemName}></Products>
+        <ItemDate
+          day={response[1].ItemDay}
+          year={response[1].ItemYear}
+        ></ItemDate>
+      </card>
     </div>
   );
 }
